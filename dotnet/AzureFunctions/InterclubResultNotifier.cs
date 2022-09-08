6546ef6d036cd041dc5e019cba33a7b2ab1c3317
Steff Beckers <steff@steffbeckers.eu>
2022-09-09 01:16:41 +0200

    Updates

--- dotnet/AzureFunctions/InterclubResultNotifier.cs
+++ dotnet/AzureFunctions/InterclubResultNotifier.cs
@@ -1,15 +1,38 @@
 using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
 namespace SnookerLimburg.AzureFunctions;
 
 public class InterclubResultNotifier
 {
+    // Timer
+    // */5 * * * * * // 5 seconds
+    // 0 */1 * * * * // 1 minute
     [FunctionName("InterclubResultNotifier")]
-    public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
+    public async Task Run([TimerTrigger("*/5 * * * * *")] TimerInfo timer, ILogger logger)
     {
-        log.LogInformation($"Interclub result notifier executed at: {DateTime.Now}");
+        logger.LogInformation($"Interclub result notifier executed at: {DateTime.Now}");
+
+        Uri baseAddress = new Uri("https://www.snookerlimburg.be");
+
+        CookieContainer cookieContainer = new CookieContainer();
+        cookieContainer.Add(baseAddress, new Cookie("jwts_tab1jwTabsCookie", "2"));
+
+        using HttpClientHandler httpClientHandler = new HttpClientHandler() { CookieContainer = cookieContainer };
+        using HttpClient httpClient = new HttpClient(httpClientHandler) { BaseAddress = baseAddress };
+
+        HttpResponseMessage response = await httpClient.GetAsync("/index.php?option=com_content&view=article&id=1&test=0&Itemid=112");
+        response.EnsureSuccessStatusCode();
+
+        string content = await response.Content.ReadAsStringAsync();
+
+        string contentMD5Hash = content.CreateMD5();
+
+        logger.LogInformation($"MD5: {contentMD5Hash}");
     }
 }
---
+++ dotnet/AzureFunctions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnookerLimburg.AzureFunctions;
+
+public static class StringExtensions
+{
+    public static string CreateMD5(this string input)
+    {
+        using MD5 md5 = MD5.Create();
+
+        byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+        byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+        return Convert.ToHexString(hashBytes);
+    }
+}
