90a6f314520b4bba13ac61f699097d66b659ef49
Steff Beckers <steff@steffbeckers.eu>
2022-09-20 20:06:38 +0200

    Initialized nx workspace

---
+++ angular/apps/interclub-result-notifier/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'snooker-limburg-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'interclub-result-notifier';
+}
