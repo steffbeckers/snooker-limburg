92da68adaf516de8e499cc7fe171623df77e8762
Steff Beckers <steff@steffbeckers.eu>
2022-09-20 20:32:03 +0200

    Service worker installation

--- angular/apps/interclub-result-notifier/src/app/app.module.ts
+++ angular/apps/interclub-result-notifier/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
