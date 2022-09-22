0e14346cc669131b070dd615a56ccd0a91652184
Steff Beckers <steff@steffbeckers.eu>
2022-09-22 21:20:04 +0200

    environment update

--- angular/apps/interclub-result-notifier/src/environments/environment.prod.ts
+++ angular/apps/interclub-result-notifier/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
   production: true,
+  endpoint: 'https://steffbeckers-snooker-limburg.azurewebsites.net/api/InterclubResultNotifierSubscriptionHub',
+  publicKey: 'BP4eujZym5wbx93H0W9jfIImBNun0GNZPA0ytd2wTeldb7cgHKYvNa1NRtJdAJ8d3e3JYojXug7AXVVh89q1HYg'
 };
